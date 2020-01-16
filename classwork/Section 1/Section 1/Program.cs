4efcee0afdba4c288216b6bc0d452ffcd6a22563
Jonathan Saysanam <jonathan.saysanam@my.tccd.edu>
2020-01-15 21:39:03 -0600

    Playing with syntax

--- classwork/Section 1/Section 1/Program.cs
+++ classwork/Section 1/Section 1/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * ITSE 1430
+ * Jonathan Saysanam 
+ */
+
+using System;
 
 namespace Section_1
 {
@@ -7,6 +12,18 @@ namespace Section_1
         static void Main ( string[] args )
         {
             Console.WriteLine("Hello World!");
+
+            int hours;
+            double payRate;
+            string name;
+            bool pass;
+
+            //logical block 1
+            int hours2 = 10;
+
+            int x, y, z;
+            int a = 10, b = 20, c = 30; 
+            //logical block 2
         }
     }
 }
