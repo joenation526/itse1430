8e6c9e39ce9517aa386d185cddcc46aac7947b1c
Jonathan <jonathan.saysanam@my.tccd.edu>
2020-02-04 16:54:59 -0600

    Worked on NewOrder

--- labs/PizzaCreator/PizzaCreator/Program.cs
+++ labs/PizzaCreator/PizzaCreator/Program.cs
@@ -12,8 +12,9 @@ namespace PizzaCreator
     class Program
     {
         // total amount of acummulated for the pizza 
-        static double total;
+        static double totalPrice;
         static bool done = false;
+
         static void Main ( string[] args )
         {
 
@@ -21,7 +22,7 @@ namespace PizzaCreator
             {
                 switch (PizzaMenu())
                 {
-                    case Command.New: break;
+                    case Command.New: NewOrder(); break;
                     case Command.Display: break;
                     case Command.Modify: break;
                     case Command.Quit: Exit(); break;
@@ -36,6 +37,8 @@ namespace PizzaCreator
             {
                 Console.WriteLine("Choose your pizza option");
                 Console.WriteLine("1. New order\n" + "2. Display order\n" + "3. Modify order\n" + "0. Quit\n");
+
+                Console.WriteLine("Current Price " + totalPrice.ToString("C"));
 
                 var selection = Console.ReadLine();
 
@@ -86,7 +89,50 @@ namespace PizzaCreator
 
         static void NewOrder ()
         {
+            Console.WriteLine("New order ");
+            Console.WriteLine("-----------");
+
+            do
+            {
+                PizzaSize();
+
+                Meats();
+
+            } while (true);
+
             
+
+        }
+
+        private static void Meats ()
+        {
+            Console.WriteLine("Meats: ($0.75 per selection)");
+            Console.WriteLine("Bacon");
+            Console.WriteLine("Ham");
+            Console.WriteLine("Pepperoni");
+            Console.WriteLine("Sausage");
+        }
+
+        private static void PizzaSize ()
+        {
+            Console.WriteLine("\nChoose your Pizza size: ");
+            Console.WriteLine("1. Small ($5)");
+            Console.WriteLine("2. Medium ($6.25)");
+            Console.WriteLine("3. Large ($7)");
+
+            string pizzaSize = Console.ReadLine();
+            const double small = 5.00;
+            const double medium = 5.00;
+            const double large = 5.00;
+
+            switch (pizzaSize)
+            {
+                case "1": totalPrice += small; break;
+                case "2": totalPrice += medium; break;
+                case "3": totalPrice += large; break;
+
+                default: Console.WriteLine("Please choose a pizza size using 1-3 num keys"); break;
+            }
         }
     }
 }
