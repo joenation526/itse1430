9fcccc17be6adbfd7dea48367f5da70e5f4d0ba9
Jonathan <jonathan.saysanam@my.tccd.edu>
2020-02-05 18:52:47 -0600

    Worked on PizzaCreator

--- labs/PizzaCreator/PizzaCreator/Program.cs
+++ labs/PizzaCreator/PizzaCreator/Program.cs
@@ -98,19 +98,96 @@ namespace PizzaCreator
 
                 Meats();
 
+                Vegetables();
+
+                Sauce();
+
+                Delivery();
+
+                break;
+
             } while (true);
+        }
 
-            
+        private static void Delivery ()
+        {
+            throw new NotImplementedException();
+        }
 
+        private static void Sauce ()
+        {
+            Console.WriteLine("Sauce: (One is Required)");
+            Console.WriteLine("1. Traditional ($0)");
+            Console.WriteLine("2. Garlic ($1)");
+            Console.WriteLine("3. Oregano ($1)");
+            Console.WriteLine("0. NEXT->");
+
+            var selection = Console.ReadLine();
+            const double pizzaSauce = 1;
+
+
+            switch (selection)
+            {
+                case "1": Console.WriteLine("Traditional Sauce Added."); break;
+                case "2": Console.WriteLine("Garlic Sauce Added."); totalPrice += pizzaSauce; break;
+                case "3": Console.WriteLine("Oregano Added."); totalPrice += pizzaSauce; break;
+
+                default: Console.WriteLine("Use the 1-4 num keys or press 0 to proceed."); break;
+            }
+        }
+
+        private static void Vegetables ()
+        {
+            Console.WriteLine("Vegetables: ($0.75 per selection)");
+            Console.WriteLine("1. Black Olives");
+            Console.WriteLine("2. Mushrooms");
+            Console.WriteLine("3. Onions");
+            Console.WriteLine("4. Peppers");
+            Console.WriteLine("0. NEXT->");
+
+            do
+            {
+                var meatSelect = Console.ReadLine();
+                const double veggies = 0.50;
+
+                switch (meatSelect)
+                {
+                    case "1": Console.WriteLine("Black Olives Added."); totalPrice += veggies; break;
+                    case "2": Console.WriteLine("Mushrooms Added."); totalPrice += veggies; break;
+                    case "3": Console.WriteLine("Onions Added."); totalPrice += veggies; break;
+                    case "4": Console.WriteLine("Peppers Added."); totalPrice += veggies; break;
+                    case "0": return;
+
+                    default: Console.WriteLine("Use the 1-4 num keys or press 0 to proceed."); break;
+                }
+            } while (true);
         }
 
         private static void Meats ()
         {
             Console.WriteLine("Meats: ($0.75 per selection)");
-            Console.WriteLine("Bacon");
-            Console.WriteLine("Ham");
-            Console.WriteLine("Pepperoni");
-            Console.WriteLine("Sausage");
+            Console.WriteLine("1. Bacon");
+            Console.WriteLine("2. Ham");
+            Console.WriteLine("3. Pepperoni");
+            Console.WriteLine("4. Sausage");
+            Console.WriteLine("0. NEXT->");
+
+            do 
+            {
+                var meatSelect = Console.ReadLine();
+                const double meats = 0.75;
+
+                switch (meatSelect)
+                {
+                    case "1": Console.WriteLine("Bacon Added."); totalPrice += meats; break;
+                    case "2": Console.WriteLine("Ham Added."); totalPrice += meats; break;
+                    case "3": Console.WriteLine("Pepperoni Added."); totalPrice += meats; break;
+                    case "4": Console.WriteLine("Sausage Added."); totalPrice += meats; break;
+                    case "0": return;
+                   
+                    default: Console.WriteLine("Use the 1-4 num keys or press 0 to proceed."); break;
+                }
+            } while (true);
         }
 
         private static void PizzaSize ()
@@ -120,19 +197,24 @@ namespace PizzaCreator
             Console.WriteLine("2. Medium ($6.25)");
             Console.WriteLine("3. Large ($7)");
 
-            string pizzaSize = Console.ReadLine();
-            const double small = 5.00;
-            const double medium = 5.00;
-            const double large = 5.00;
 
-            switch (pizzaSize)
+            do
             {
-                case "1": totalPrice += small; break;
-                case "2": totalPrice += medium; break;
-                case "3": totalPrice += large; break;
+                string pizzaSize = Console.ReadLine();
+                const double small = 5.00;
+                const double medium = 5.00;
+                const double large = 5.00;
 
-                default: Console.WriteLine("Please choose a pizza size using 1-3 num keys"); break;
-            }
+                switch (pizzaSize)
+                {
+                    case "1": totalPrice += small; return;
+                    case "2": totalPrice += medium; return;
+                    case "3": totalPrice += large; return;
+
+                    default: Console.WriteLine("Please choose a pizza size using 1-3 num keys"); break;
+                }
+            } while (true);
+            
         }
     }
 }
